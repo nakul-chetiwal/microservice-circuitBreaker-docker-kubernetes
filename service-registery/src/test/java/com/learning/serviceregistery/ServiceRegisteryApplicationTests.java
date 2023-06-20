8ead7e04139340e1e365d98c62baa6dc901f0682
nakul chetiwal <chetiwalnakul7@gmail.com>
2023-06-20 10:47:03 -0400

    Service Registry Server to register all the client service

---
+++ service-registery/src/test/java/com/learning/serviceregistery/ServiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.serviceregistery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
